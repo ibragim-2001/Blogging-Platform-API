8ef9c1c87a4896f8eeb5eda5245741aa1c740443
ibragim-2001 <saitamadesign7@gmail.com>
2024-09-27 23:36:00 +0300

    exception view

--- blog/views.py
+++ blog/views.py
@@ -9,13 +9,19 @@ from .models import *
 class PostsView(APIView):
 
     def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
+        try:
+            posts = Post.objects.all()
+            serializer = PostSerializer(posts, many=True)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_404_NOT_FOUND)
 
     def post(self, request):
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
+            try:
+                serializer.save()
+                return Response(status=status.HTTP_201_CREATED)
+            except Exception as e:
+                return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
