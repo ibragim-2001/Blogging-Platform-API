99b46c3c7526e5b877439887055b2bbc847abc1e
ibragim-2001 <saitamadesign7@gmail.com>
2024-09-28 18:32:32 +0300

    end

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,17 +1,22 @@
 from rest_framework import serializers
+
 from .models import *
 
 
 class PostSerializer(serializers.ModelSerializer):
+    category = serializers.CharField()
 
     class Meta:
         model = Post
         fields = ('title', 'content', 'category', 'tags', 'create_at', 'update_at')
         read_only_fields = ('create_at', 'update_at')
 
+
     def create(self, validated_data):
         tags = validated_data.pop('tags')
-        post = Post.objects.create(**validated_data)
+        category_name = validated_data.pop('category')
+        category = Category.objects.get(title=category_name)
+        post = Post.objects.create(category=category, **validated_data)
         post.tags.set(tags)
         return tags
 
